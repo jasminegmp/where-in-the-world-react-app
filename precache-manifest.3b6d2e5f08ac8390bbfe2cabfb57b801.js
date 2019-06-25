2aae76f9c05101e39f241775d667f7f428380c89
jasminegmp <jasminekgump@gmail.com>
2019-06-25 16:25:54 -0700

    Updates

---
+++ precache-manifest.3b6d2e5f08ac8390bbfe2cabfb57b801.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e17380a562d5dd36dcab1c0563db4e",
+    "url": "/jasminegmp.github.io/where-in-the-world-react-app/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/jasminegmp.github.io/where-in-the-world-react-app/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "70b5bc6afa4b2101ef88",
+    "url": "/jasminegmp.github.io/where-in-the-world-react-app/static/js/main.77455247.chunk.js"
+  },
+  {
+    "revision": "e35c91f07a813d307b2c",
+    "url": "/jasminegmp.github.io/where-in-the-world-react-app/static/js/runtime~main.b5103418.js"
+  }
+]);
