b259320ef83d596b089411680a1f1ee945a784c6
jasminegump <jasmine.kim.18@gmail.com>
2019-06-25 19:32:06 -0700

    Rest Countries API linked

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchBar from './SearchBar'
+import axios from 'axios';
 
 class App extends React.Component {
 
@@ -8,9 +9,20 @@ class App extends React.Component {
 		selectedCountry: null
 	};
 
+	// loads all the countries
+	async componentDidMount(){
+		const link = `https://restcountries.eu/rest/v2/all`;
+		const response = await axios.get(link);
+		console.log(response);
+		this.setState({countries: response.data});
+	}
 
+	// loads only countries on submit
 	onCountrySubmit = async country => {
-		console.log(country);
+		const link = `https://restcountries.eu/rest/v2/name/${country}`;
+		const response = await axios.get(link);
+		console.log(response);
+		this.setState({countries: response.data});
 	};
 
 
